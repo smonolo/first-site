f822fc1f4590cb1468f8f62c3ba7be491b46a21b
s.monolo <lu3mtex@gmail.com>
2020-12-06 14:44:35 +0100

    remove headers stuff

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-  next();
-});
-
 app.use('/api/app', require('./routes/api/app'));
 
 app.get('*', (req, res) => {
