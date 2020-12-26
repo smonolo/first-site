857000bf88d026dba3007b8fdb92831558f24ec4
s.monolo <lu3mtex@gmail.com>
2020-12-26 16:35:36 +0100

    remove snowfall

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import axios, { AxiosResponse } from 'axios';
-import Snowfall from 'react-snowfall';
+// import Snowfall from 'react-snowfall';
 
 import { pages, Page } from './constants';
 
@@ -51,9 +51,9 @@ class App extends Component<Props> {
     return (
       <BrowserRouter>
         <GlobalStyle/>
-        <Snowfall
+        {/* <Snowfall
           snowflakeCount={50}
-        />
+        /> */}
         <Switch>
           {pages.map((page: Page, index: number) => (
             <Route
